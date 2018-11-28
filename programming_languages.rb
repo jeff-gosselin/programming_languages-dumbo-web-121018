7820b4d6567b849231e178fb8c1934b6c41d2fe4
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-28 06:08:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  lang_list = languages.map {|key, val| val.keys}.flatten.uniq
+  style_list = languages.keys
+  
+  lang_list.each {|lang| new_hash[lang] = {}}
+  
+  languages.each do |x, y|
+    
+  binding.pry 
+  end
+  
+  
+  new_hash
 end
